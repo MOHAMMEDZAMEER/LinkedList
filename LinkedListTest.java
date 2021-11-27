17113f1c6b92340475a1b91702a71197af584c0a
MOHAMMEDZAMEER <zameer4939@gmail.com>
2021-11-27 05:45:30 -0400

    Create LinkedList with 3Nodes

---
+++ LinkedListTest.java
@@ -0,0 +1,16 @@
+package com.LinledList;
+
+public class LinkedListTest {
+
+	public static void main(String[] args) {
+		
+		Node node1= new Node(56);
+		Node node2= new Node(30);
+		Node node3= new Node(70);
+		
+		
+		node1.next=node2;
+		node2.next=node3;
+	}
+
+}
---
+++ Node.java
@@ -0,0 +1,12 @@
+package com.LinledList;
+
+public class Node {
+
+	int data;
+	Node next = null;
+	public Node(int node) {
+		super();
+		this.data = data;
+	}
+	
+}
