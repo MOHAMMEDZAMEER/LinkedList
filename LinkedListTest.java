523a170ba7b17f011914b7b92d04426d4eba6c97
MOHAMMEDZAMEER <zameer4939@gmail.com>
2021-11-28 02:33:30 -0400

    Add method to delete last Node

--- LinkedListTest.java
+++ LinkedListTest.java
@@ -3,14 +3,15 @@ package com.LinledList;
 public class LinkedListTest {
 
 	public static void main(String[] args) {
-		
-LinkedListServices list = new LinkedListServices();
+
+		LinkedListServices list = new LinkedListServices();
 		
 		list.insertAtHead(56);
+		list.append(30);
 		list.append(70);
 		list.displayLinkedList();
 		
-		list.insertBetween(56, 30);
+		list.popLast();
 		list.displayLinkedList();
 	}
 }
